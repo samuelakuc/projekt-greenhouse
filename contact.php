681f20470e9b4789fdde6273fe740235e89baea1
samuelakuc <samuel.akuc@student.ukf.sk>
2025-04-03 17:15:09 +0200

    database commit

--- contact.php
+++ contact.php
@@ -54,17 +54,17 @@ https://templatemo.com/tm-539-simple-house
 			<div class="tm-container-inner-2 tm-contact-section">
 				<div class="row">
 					<div class="col-md-6">
-						<form action="" method="POST" class="tm-contact-form">
+						<form action="database/databaza_formular.php" method="POST" class="tm-contact-form">
 					        <div class="form-group">
-					          <input type="text" name="name" class="form-control" placeholder="Name" required="" />
+					          <input type="text" name="name" id="name" class="form-control" placeholder="Name" required="" />
 					        </div>
 					        
 					        <div class="form-group">
-					          <input type="email" name="email" class="form-control" placeholder="Email" required="" />
+					          <input type="email" name="email" id="email" class="form-control" placeholder="Email" required="" />
 					        </div>
 				
 					        <div class="form-group">
-					          <textarea rows="5" name="message" class="form-control" placeholder="Message" required=""></textarea>
+					          <textarea rows="5" name="message" id="message" class="form-control" placeholder="Message" required=""></textarea>
 					        </div>
 					
 					        <div class="form-group tm-d-flex">
---
+++ database/databaza_formular.php
@@ -0,0 +1,45 @@
+<?php
+// PDO databázové pripojenie
+$host = "localhost";
+$dbname = "formulár";
+$port = 3306;
+$username = "root";
+$password = "";
+
+// Možnosti
+
+$options = array(
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+);
+
+// Pripojenie PDO
+
+try {
+    $conn = new PDO('mysql:host='.$host.';dbname='.$dbname.";port=".$port, $username,
+        $password, $options);
+} catch (PDOException $e) {
+    die("Chyba pripojenia: " . $e->getMessage());
+}
+
+$name = $_POST["name"];
+$email = $_POST["email"];
+$message = $_POST["message"];
+
+// SQL príkaz INSERT
+$sql = "INSERT INTO udaje (meno, email, sprava) VALUES (:name, :email, :message)";
+$statement = $conn->prepare($sql);
+
+$statement->bindParam(':name', $name);
+$statement->bindParam(':email', $email);
+$statement->bindParam(':message', $message);
+
+try {
+    $insert = $statement->execute();
+    return $insert;
+} catch (\Exception $exception) {
+    return false;
+}
+// Zatvorenie pripojenia
+$conn = null;
+
